f25e69bf87e3a20ad4bf7da8670d1104fc6a0509
paradoxfluxgd2 <195960623+paradoxflux2@users.noreply.github.com>
2025-01-24 18:03:19 -0300

    modify 'set_origin' to use destination instead of origin

--- src/gddt.py
+++ src/gddt.py
@@ -12,21 +12,55 @@ import transfersave
 root = tk.Tk()
 
 origin = None
+label = None
 
-def change_msg(new_message):
-    """change message in window and print same message"""
-    print(new_message)
-    label.config(text = new_message)
+def create_ui():
+    global label
 
-# button functions
+    # create a menubar
+    menubar = tk.Menu(root)
+    root.config(menu=menubar)
+
+    help_menu = tk.Menu(menubar, tearoff=False)
+
+    # Help menu buttons
+    help_menu.add_command(label='Settings', command=open_settings)
+    help_menu.add_command(label='Exit', command=root.destroy)
+
+    # add the Help menu to the menubar
+    menubar.add_cascade(label="Help", menu=help_menu)
+
+    # title
+    title = tk.Label(root, text="GD Data Transfer", font=('Arial', 18))
+    title.pack(padx=20, pady=20)
+
+    # message
+    label = tk.Label(root, text="please select a destination first", font=('Arial', 12))
+    label.pack(side=tk.BOTTOM, padx=20, pady=20)
+
+    # transfer button
+    transfer_button = tk.Button(root, text='Transfer', command=transfer_button_click)
+    transfer_button.pack(side=tk.BOTTOM)
+
+    # phone to computer button
+    phone_button = tk.Button(root, text='Phone to computer', command=phone_button_click)
+    phone_button.pack(pady=3)
+
+    # computer to phone button
+    pc_button = tk.Button(root, text='Computer to phone', command=pc_button_click)
+    pc_button.pack(pady=3)
+
+direction = {"phone": "computer", "computer": "phone"} # if source is phone, destination is computer, etc
 
 def set_origin(device):
     global origin
     if origin == device: # extra useless code yay
-        change_msg(f"destination was already {device}, are you stupid?")
+        change_msg(f"destination was already {direction[origin]}, are you stupid?")
     else:
         origin = device
-        change_msg(f"changed destination to {device}")
+        change_msg(f"changed destination to {direction[origin]}")
+
+# button functions
 
 def phone_button_click():
     set_origin("phone")
@@ -44,6 +78,12 @@ def transfer_button_click():
             change_msg("save files transferred succesfully!")
         else:
             change_msg(f"couldnt transfer files\n{result.stderr.strip()}")
+
+
+def change_msg(new_message):
+    """change message in window and print same message"""
+    print(new_message)
+    label.config(text = new_message)
 
 # settings
 
@@ -103,42 +143,6 @@ def start_adb_server():
     start_server_command = [str(transfersave.path_adb), "start-server"]
     subprocess.run(start_server_command, capture_output=True, text=True, check=False)
     change_msg("adb server started")
-
-def create_ui():
-    global label
-
-    # create a menubar
-    menubar = tk.Menu(root)
-    root.config(menu=menubar)
-
-    help_menu = tk.Menu(menubar, tearoff=False)
-
-    # Help menu buttons
-    help_menu.add_command(label='Settings', command=open_settings)
-    help_menu.add_command(label='Exit', command=root.destroy)
-
-    # add the Help menu to the menubar
-    menubar.add_cascade(label="Help", menu=help_menu)
-
-    # title
-    title = tk.Label(root, text="GD Data Transfer", font=('Arial', 18))
-    title.pack(padx=20, pady=20)
-
-    # message
-    label = tk.Label(root, text="please select a destination first", font=('Arial', 12))
-    label.pack(side=tk.BOTTOM, padx=20, pady=20)
-
-    # transfer button
-    transfer_button = tk.Button(root, text='Transfer', command=transfer_button_click)
-    transfer_button.pack(side=tk.BOTTOM)
-
-    # phone to computer button
-    phone_button = tk.Button(root, text='Phone to computer', command=phone_button_click)
-    phone_button.pack(pady=3)
-
-    # computer to phone button
-    pc_button = tk.Button(root, text='Computer to phone', command=pc_button_click)
-    pc_button.pack(pady=3)
 
 def main():
     root.title("GD Data Transfer")
